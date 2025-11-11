0636a50b39f0b16030bae8b5c4805e89514c02f5
dhmh204 <daohoangminhhang24@gmail.com>
2025-11-11 15:12:53 +0700

    init prj

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from './login.module.scss';
+function Login() {
+  return (
+    <h1>Login</h1>
+  )
+}
+
+export default Login
---
+++ src/routes/routes.js
@@ -0,0 +1,19 @@
+import config from '~/config';
+
+// Layouts
+
+// Pages
+
+// Public routes
+// const publicRoutes = [
+//     { path: config.routes.home, component: Home },
+//     { path: config.routes.following, component: Following },
+//     { path: config.routes.live, component: Live },
+//     { path: config.routes.profile, component: Profile },
+//     { path: config.routes.upload, component: Upload, layout: HeaderOnly },
+//     { path: config.routes.search, component: Search, layout: null },
+// ];
+
+// const privateRoutes = [];
+
+// export { publicRoutes, privateRoutes };
